18979315349f07f93a9acd3d77924125dce6c9d4
Fábio Ferreira <ffcfpten@gmail.com>
2020-01-23 04:08:46 +0000

    chores: updater script

---
+++ update-organiser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Updating your server tools..."
+
+
+cd ~/code/organiser;
+git pull > /dev/null 2>&1;
+
+echo "Scripts updated"
+
+# add execution rights
+chmod +x *.py
+cp *.py /usr/local/bin
+
+exit 0
